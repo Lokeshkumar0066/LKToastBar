5c077b79b2c07f5aee6801c36ca977427fcf16e7
Lokesh Kumar <lokesh.kumar@iifl.com>
2018-08-10 10:50:32 +0530

    Initial Commit

---
+++ ToastBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToastBar
+//
+//  Created by Apple on 10/08/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToastBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToastBar
+//
+//  Created by Apple on 10/08/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
